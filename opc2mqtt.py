79b3fe56b484e60ec09a7bad833c37c1b270f0a3
lytkarinskiy <asnaumoff@gmail.com>
2017-03-18 16:08:14 +0000

    move to pycharm

--- opc2mqtt.py
+++ opc2mqtt.py
@@ -6,6 +6,7 @@ import paho.mqtt as mqtt
 import OpenOPC
 from collections import OrderedDict
 
+
 def do_publish(client):
     """Internal function"""
     m = client._userdata.pop()
@@ -29,12 +30,14 @@ def do_publish(client):
         raise ValueError('message must be a dict or a tuple')
     client.publish(topic, payload, qos, retain)
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-	if rc == 0:
-		do_publish(client)
-	else:
-		raise mqtt.MQTTException(paho.connack_string(rc))
+    if rc == 0:
+        do_publish(client)
+    else:
+        raise mqtt.MQTTException(paho.connack_string(rc))
+
 
 # The callback for when message that was to be sent using the publish() call has completed transmission to the broker.
 def on_publish(client, userdata, mid):
@@ -43,56 +46,60 @@ def on_publish(client, userdata, mid):
     else:
         do_publish(client)
 
+
 # The callback for when the client disconnects from the broker.
 def on_disconnect(client, userdata, rc):
     if rc != 0:
         print("on_disconnect: Unexpected disconnection")
 
+
 def connectToOPCServer(opcHostName, opcServerName, mode):
-	# connect to remote OpenOPC gateway
-	if mode == "open":
-		try:
-			opc = OpenOPC.open_client(opcHostName)
-		except:
-			print "Unknown host"
-	elif mode == "dcom":
-		try:
-			opc = OpenOPC.client(opcHostName)
-		except:
-			print "Unknown host"
-	else:
-		print "Unknown connection mode"
+    # connect to remote OpenOPC gateway
+    if mode == "open":
+        try:
+            opc = OpenOPC.open_client(opcHostName)
+        except:
+            print "Unknown host"
+    elif mode == "dcom":
+        try:
+            opc = OpenOPC.client(opcHostName)
+        except:
+            print "Unknown host"
+    else:
+        print "Unknown connection mode"
 
+    # Connect to the OPC server inside Gateway
+    try:
+        opc.connect(opcServerName)
+        print "Connected to OpenOPC server " + opcServerName + " on host " + opcHostName
+    except:
+        print "Unknown OPC Server"
+    return opc
 
-	# Connect to the OPC server inside Gateway
-	try:
-		opc.connect(opcServerName)
-		print "Connected to OpenOPC server " + opcServerName + " on host " + opcHostName
-	except:
-		print "Unknown OPC Server"
-	return opc
 
 def opcCreateReadList(opcConnection, mask):
-	return opcConnection.list(mask, flat=True)
+    return opcConnection.list(mask, flat=True)
+
 
 def opcJsonPayload(opcConnection, opcReadList, spec):
-	payload = ""
-	if spec == "tekon_water":
-		for name, value, quality, dtime in opcConnection.read(opcReadList):
-			# Convert OpenOPC datetime format to ISO8601
-			# 
-			# CONVERT TO UTC
-			# 
-			dtime = time.strftime('%Y-%m-%dZ%H:%M:%ST', time.strptime(dtime, "%m/%d/%y %H:%M:%S"))
-			# Convert "quality" to true/false
-			quality = True if quality=="Good" else False
-			# Use OrderedDict to save JSON keys order
-			dataJSON = OrderedDict([("_spec", "tekon_water"), ("value", int(value)),("quality", quality)])
-			fullJSON = OrderedDict([("meterDescription", name),("recievedDate", dtime),("data", dataJSON)])
-			payload += json.dumps(fullJSON, indent = 4)
-	else:
-		print "Unknown specification"
-	return payload
+    payload = ""
+    if spec == "tekon_water":
+        for name, value, quality, dtime in opcConnection.read(opcReadList):
+            # Convert OpenOPC datetime format to ISO8601
+            #
+            # CONVERT TO UTC
+            #
+            dtime = time.strftime('%Y-%m-%dZ%H:%M:%ST', time.strptime(dtime, "%m/%d/%y %H:%M:%S"))
+            # Convert "quality" to true/false
+            quality = True if quality == "Good" else False
+            # Use OrderedDict to save JSON keys order
+            dataJSON = OrderedDict([("_spec", "tekon_water"), ("value", int(value)), ("quality", quality)])
+            fullJSON = OrderedDict([("meterDescription", name), ("recievedDate", dtime), ("data", dataJSON)])
+            payload += json.dumps(fullJSON, indent=4)
+    else:
+        print "Unknown specification"
+    return payload
+
 
 opc_host, opc_server, mqtt_broker_host, mqtt_client_id = [line.strip() for line in open("settings", 'r').readlines()]
 # The loop for periodical read and publish
@@ -100,21 +107,22 @@ updateRate = 5
 topic = "odintcovo/water"
 
 while True:
-	# Create connection to OPC server and read vars
-	opcConnection = connectToOPCServer(opc_host, opc_server, "open")
-	opcReadList = opcCreateReadList(opcConnection, 'Random.*Int*')
-	payload = opcJsonPayload(opcConnection, opcReadList, "tekon_water")
+    # Create connection to OPC server and read vars
+    opcConnection = connectToOPCServer(opc_host, opc_server, "open")
+    opcReadList = opcCreateReadList(opcConnection, 'Random.*Int*')
+    payload = opcJsonPayload(opcConnection, opcReadList, "tekon_water")
 
-	msg = {'topic':topic, 'payload':payload, 'qos':1}
-	mqttc = paho.Client(client_id=mqtt_client_id, userdata = [msg])
-	mqttc.on_connect = on_connect
-	mqttc.on_publish = on_publish
-	mqttc.on_disconnect = on_disconnect
-	mqttc.tls_set(ca_certs = 'ca.crt', certfile = 'client_cert.pem', keyfile = 'client_key.pem', tls_version=ssl.PROTOCOL_TLSv1_2)
-	mqttc.tls_insecure_set(True)     # prevents ssl.SSLError: Certificate subject does not match remote hostname.
-	mqttc.connect(mqtt_broker_host, port = 8883, keepalive = 10)
-	mqttc.loop_forever()
+    msg = {'topic': topic, 'payload': payload, 'qos': 1}
+    mqttc = paho.Client(client_id=mqtt_client_id, userdata=[msg])
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_disconnect = on_disconnect
+    mqttc.tls_set(ca_certs='ca.crt', certfile='client_cert.pem', keyfile='client_key.pem',
+                  tls_version=ssl.PROTOCOL_TLSv1_2)
+    mqttc.tls_insecure_set(True)  # prevents ssl.SSLError: Certificate subject does not match remote hostname.
+    mqttc.connect(mqtt_broker_host, port=8883, keepalive=10)
+    mqttc.loop_forever()
 
-	opcConnection.close()
-	print "Connection to OpenOPC server " + opc_server + " on host " + opc_host + " is closed"
-	time.sleep(updateRate)
+    opcConnection.close()
+    print "Connection to OpenOPC server " + opc_server + " on host " + opc_host + " is closed"
+    time.sleep(updateRate)
