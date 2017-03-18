8876551982d122400ec3aaabe745542438b53e20
lytkarinskiy <asnaumoff@gmail.com>
2017-03-18 15:04:45 +0000

    seems to work

--- opc2mqtt.py
+++ opc2mqtt.py
@@ -1,32 +1,47 @@
 import time
 import json
-import OpenOPC
 import ssl
+import paho.mqtt.client as paho
+import paho.mqtt as mqtt
+import OpenOPC
 from collections import OrderedDict
-import paho.mqtt.client as mqtt
+
+def do_publish(c):
+    """Internal function"""
+    m = c._userdata.pop()
+    if type(m) is dict:
+        topic = m['topic']
+        try:
+            payload = m['payload']
+        except KeyError:
+            payload = None
+        try:
+            qos = m['qos']
+        except KeyError:
+            qos = 0
+        try:
+            retain = m['retain']
+        except KeyError:
+            retain = False
+    elif type(m) is tuple:
+        (topic, payload, qos, retain) = m
+    else:
+        raise ValueError('message must be a dict or a tuple')
+    c.publish(topic, payload, qos, retain)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-	msg = ""
 	if rc == 0:
-		msg = "Connection successful" 
-	elif rc == 1:
-		msg = "Connection refused - incorrect protocol version"
-	elif rc == 2:
-		msg = "Connection refused - invalid client identifier"
-	elif rc == 3:
-		msg = "Connection refused - server unavailable"
-	elif rc == 4:
-		msg = "Connection refused - bad username or password"
-	elif rc == 5:
-		msg = "Connection refused - not authorised" 
+		do_publish(client)
 	else:
-		msg = "Unknown error"
-	print "on_connect: " + str(msg)
+		raise mqtt.MQTTException(paho.connack_string(rc))
 
 # The callback for when message that was to be sent using the publish() call has completed transmission to the broker.
 def on_publish(client, userdata, mid):
-    print "on_publish: mid "+str(mid)
+    if len(userdata) == 0:
+        client.disconnect()
+    else:
+        do_publish(client)
 
 # The callback for when the client disconnects from the broker.
 def on_disconnect(client, userdata, rc):
@@ -56,36 +71,23 @@ except:
 readList = opc.list('Random.Int*', flat = True)
 
 # Define MQTT broker/client properties
-topic = "odintcovo/water"
-qos=1
 hostname=hostname
 port=8883
 client_id=client_id
+topic = "odintcovo/water"
+qos=1
+retain = False
 
-debug = True
-mqttc = mqtt.Client(client_id)
-if debug:
-	mqttc.on_connect = on_connect
-	mqttc.on_publish = on_publish
-	mqttc.on_disconnect = on_disconnect
-mqttc.tls_set(ca_certs = homedir + 'ca.crt', 
-	certfile = homedir + 'client_cert.pem', 
-	keyfile = homedir + 'client_key.pem', 
-	tls_version=ssl.PROTOCOL_TLSv1_2)
-mqttc.tls_insecure_set(True)     # prevents ssl.SSLError: Certificate subject does not match remote hostname.
-mqttc.connect(hostname, port)
-mqttc.loop_start()
+
 
 # The loop for periodical read and publish
-updateRate = 3
+updateRate = 15
 while True:
+	payload = ""
 	for name, value, quality, dtime in opc.read(readList):
 		# Convert OpenOPC datetime format to ISO8601
 		# 
-		# 
 		# CONVERT TO UTC
-		# 
-		# 
 		# 
 		dtime = time.strftime('%Y-%m-%dZ%H:%M:%ST', time.strptime(dtime, "%m/%d/%y %H:%M:%S"))
 		# Convert "quality" to true/false
@@ -93,14 +95,22 @@ while True:
 		# Use OrderedDict to save JSON keys order
 		dataJSON = OrderedDict([("_spec", "tekon_water"), ("value", int(value)),("quality", quality)])
 		fullJSON = OrderedDict([("meterDescription", name),("recievedDate", dtime),("data", dataJSON)])
-		payload = json.dumps(fullJSON, indent = 4)
-		# print payload
-	mqttc.publish(topic, payload, qos)
+		payload += json.dumps(fullJSON, indent = 4)
+	# print payload
+	msg = {'topic':topic, 'payload':payload, 'qos':qos, 'retain':retain}
+	# print msg
+	mqttc = paho.Client(client_id=client_id, userdata = [msg])
+	# print mqttc._userdata
+	mqttc.on_connect = on_connect
+	mqttc.on_publish = on_publish
+	mqttc.on_disconnect = on_disconnect
+	mqttc.tls_set(ca_certs = homedir + 'ca.crt', 
+		certfile = homedir + 'client_cert.pem', 
+		keyfile = homedir + 'client_key.pem', 
+		tls_version=ssl.PROTOCOL_TLSv1_2)
+	mqttc.tls_insecure_set(True)     # prevents ssl.SSLError: Certificate subject does not match remote hostname.
+	mqttc.connect(hostname, port, keepalive = 10)
+	mqttc.loop_forever()
 	time.sleep(updateRate)
-
 opc.close()
 print "OPC closed"
-mqttc.loop_stop()
-print "loop stopped"
-mqttc.disconnect()
-print "MQTT disconnected"
